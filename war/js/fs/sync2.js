0be55a089e223e3dafb80cbca453e029f814862c
hoge1e3 <hoge1e3@gmail.com>
2015-10-07 11:16:31 +0900

    fs2-1

--- war/js/fs/sync2.js
+++ war/js/fs/sync2.js
@@ -26,6 +26,62 @@ define(["FS","Shell",/*"requestFragment",*/"WebSite","SFile","assert"],
         // sync dir:file options:o local=remote=dir
         // sync local:file remote:file options:o
         var local,remote,options;
+        function getLocalDirInfo() {
+            var res={};
+            local.recursive(function (file) {
+                var lcm=file.metaInfo();
+                res[file.relPath(local)]=lcm;
+            },{excludes:options.excludes});
+            return res;
+        }
+        function unionKeys() {
+            var keys={};
+            for (var i=0 ; i<arguments.length ;i++) {
+                for (var key in arguments[i]) {keys[key]=1;}
+            }
+            return keys;
+        }
+        function getDelta(before,after) {
+            //console.log("getDelta",before,after);
+            var keys=unionKeys(before,after);
+            var res={};
+            for (var key in keys) {
+                var inb=(key in before),ina=(key in after);
+                //console.log("Compare", before[key], after[key], ina, inb);
+                if (inb && !ina) {
+                    // DELETED
+                    res[key]={lastUpdate:-1, trashed:true};
+                } else if (!inb && ina) {
+                    // CREATEDED
+                    res[key]={lastUpdate:after[key].lastUpdate, created:true};
+                } else if (before[key].lastUpdate != after[key].lastUpdate) {
+                    // MODIFIED
+                    res[key]={
+                            lastUpdate: after[key].lastUpdate,
+                            modified:true
+                    };
+                    //console.log("Added", key, before[key].lastUpdate , after[key].lastUpdate)
+                }
+            }
+            return res;
+        }
+        function getDeltaDelta(local,remote) {
+            var keys=unionKeys(local,remote);
+            var res={local:{}, remote:{} };
+            for (var key in keys) {
+                var inl=(key in local),inr=(key in remote);
+                if (inl && !inr) {
+                    res.local[key]=local[key];
+                } else if (!inl && inr) {
+                    res.remote[key]=remote[key];
+                } else if (local[key].lastUpdate > remote[key].lastUpdate) {
+                    res.local[key]=local[key];
+                } else {
+                    res.remote[key]=remote[key];
+                }
+            }
+            return res;
+        }
         var i=0;
         if (SFile.is(arguments[i])) {
             local=arguments[i];
@@ -40,7 +96,18 @@ define(["FS","Shell",/*"requestFragment",*/"WebSite","SFile","assert"],
         if (!remote) remote=local;
         if (!options) options={};
         if (options.test) options.v=1;
+        var syncInfoDir=local.rel(".sync/");
+        options.excludes=options.excludes||[];
+        options.excludes=options.excludes.concat(syncInfoDir.name());
+        var localDirInfoFile=syncInfoDir.rel("local.json");
+        var remoteDirInfoFile=syncInfoDir.rel("remote.json");
+        var lastLocalDirInfo=localDirInfoFile.exists()?localDirInfoFile.obj():{};
+        var lastRemoteDirInfo=remoteDirInfoFile.exists()?remoteDirInfoFile.obj():{};
+        var curLocalDirInfo=getLocalDirInfo();
+        var localDelta=getDelta(lastLocalDirInfo, curLocalDirInfo);
+        if (options.v) sh.echo("localDelta",localDelta);
         var uploads={},downloads=[],visited={};
+        var user;
         function status(name, param) {
             sh.echo("Status: "+name+" param:",param);
             if (options.onstatus) {
@@ -58,31 +125,37 @@ define(["FS","Shell",/*"requestFragment",*/"WebSite","SFile","assert"],
             type:"get",
             url:A(WebSite.url.getDirInfo),
             data:req
-        }).then(function n1(info) {
-            //info=JSON.parse(info);
+        }).then(function n1(curRemoteDirInfo) {
             var d;
-            if (options.v) sh.echo("getDirInfo",info);
-            if (info.NOT_LOGGED_IN) {
+            if (options.v) sh.echo("getDirInfo",curRemoteDirInfo);
+            if (curRemoteDirInfo.NOT_LOGGED_IN) {
                 d = new $.Deferred;
                 setTimeout(function(){
                   d.reject(Sync.NOT_LOGGED_IN);
                 }, 0);
                 return d.promise();
             }
-            var base=local;//FS.get(info.base);
-            var data=info.data;
-            for (var rel in data) {
-                var file=base.rel(rel);
-                var lcm=file.exists({includeTrashed:true}) ? file.metaInfo() : null;
-                var rmm=data[rel];
-                cmp(file,rel,lcm,rmm);
+            user=curRemoteDirInfo.user;
+            var base=local;//FS.get(curRemoteDirInfo.base);
+            var remoteDelta=getDelta(lastRemoteDirInfo, curRemoteDirInfo.data);
+            if (options.v) sh.echo("remoteDelta",remoteDelta);
+            var dd=getDeltaDelta(localDelta,remoteDelta);
+            var o,f,m;
+            for (var key in dd.local) {
+                 f=local.rel(key);
+                 o={};
+                 if (f.exists()) o.text=f.text();
+                 m=dd.local[key];
+                 for (var i in m) o[i]=m[i];
+                 uploads[key]=o;
+                 if (options.v)
+                        sh.echo("Upload",key,m);
             }
-            local.recursive(function (file) {
-                var lcm=file.metaInfo();
-                var rel=file.relPath(local);
-                var rmm=data[rel];
-                cmp(file,rel,lcm,rmm);
-            },{includeTrashed:true, excludes:options.excludes});
+            for (var key in dd.remote) {
+                downloads.push(key);
+                if (options.v)
+                    sh.echo("Download",key,dd.remote[key]);
+            }
             if (options.v) {
                 sh.echo("uploads:",uploads);
                 sh.echo("downloads:",downloads);
@@ -121,36 +194,16 @@ define(["FS","Shell",/*"requestFragment",*/"WebSite","SFile","assert"],
                 data:req
             });
         }).then(function n3(res){
+            var newRemoteDirInfo=res.data;
             if (options.v) sh.echo("putFiles res=",res);
+            var newLocalDirInfo=getLocalDirInfo();
+            localDirInfoFile.obj(newLocalDirInfo);
+            remoteDirInfoFile.obj(newRemoteDirInfo);
+
             var upds=[];
             for (var i in uploads) upds.push(i);
-            return res={msg:res,uploads:upds,downloads: downloads};
+            return res={msg:res,uploads:upds,downloads: downloads,user:user};
         });
-        function cmp(f,rel,lcm,rmm) {
-            if (options.v) console.log("compare",f.path(), rel,lcm,rmm);
-            if (visited[rel]) return ;
-            visited[rel]=1;
-            if (rmm && (!lcm || lcm.lastUpdate<rmm.lastUpdate-5000)) {
-                downloads.push(rel);
-                if (options.v)
-                    sh.echo((!lcm?"New ":"")+
-                            "mtime(l-r) "+( (lcm?lcm.lastUpdate:0)-rmm.lastUpdate)+
-                            " Download "+f+
-                            " trash="+!!rmm.trashed);
-            } else if (lcm && (!rmm || lcm.lastUpdate>rmm.lastUpdate+5000)) {
-                var o={};
-                if (f.exists()) o.text=f.text();
-                var m=f.metaInfo();
-                for (var i in m) o[i]=m[i];
-                uploads[rel]=o;
-                if (options.v)
-                    sh.echo((!rmm?"New":"")+
-                            "mtime(l-r) "+(lcm.lastUpdate-(rmm?rmm.lastUpdate:0))+
-                            " Upload "+f+
-                            " trash="+!!lcm.trashed);
-            }
-
-        }
     };
     sh.rsh=function () {
         var a=[];
